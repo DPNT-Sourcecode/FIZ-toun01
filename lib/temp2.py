398697adbb2fda4632bd16516347d0fddcdf2637
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 16:15:15 +0000

    Tue Aug 27 16:15:15 UTC 2019

--- lib/temp2.py
+++ lib/temp2.py
@@ -7,11 +7,13 @@ Created on Tue Aug 27 16:54:51 2019
 
 def fizz_buzz(number):
     
-   if number%3 == 0:
+   if number%3 == 0 and number%5 == 0: 
+          return 'fizz buzz'
+   elif number%3 == 0:
           return 'fizz'
    elif number%5 == 0:
           return 'buzz'   
    else:
         return number
 if __name__ == '__main__':
-   print(fizz_buzz(30))
+   print(fizz_buzz(16))
