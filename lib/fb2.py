5dd278afa226fb4764d2ea09c4608ed0759cdef1
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 16:24:21 +0000

    Tue Aug 27 16:24:21 UTC 2019

---
+++ lib/fb2.py
@@ -0,0 +1,13 @@
+
+def fizz_buzz2(number):
+    
+   if number%3 == 0 and number%5 == 0: 
+          return 'fizz buzz'
+   elif number%3 == 0:
+          return 'fizz'
+   elif number%5 == 0:
+          return 'buzz'   
+   else:
+        return number
+if __name__ == '__main__':
+   print(fizz_buzz(16))
