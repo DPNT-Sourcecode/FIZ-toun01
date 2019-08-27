5364e0a08c508417020426c7bdaf675631dbe15f
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 16:36:21 +0000

    Tue Aug 27 16:36:21 UTC 2019

--- lib/fb2.py
+++ lib/fb2.py
@@ -1,19 +1,19 @@
 
 def fizz_buzz2(number):
     
-   #if number%3 == 0 and number%5 == 0: 
-   #       return 'fizz buzz'
-   #elif number%3 == 0:
-   #       return 'fizz'
-   #elif number%5 == 0:
-   #       return 'buzz'   
-   
-   if str(number).__contains__('3'):
+   if number%3 == 0 and number%5 == 0: 
+          return 'fizz buzz'
+   elif number%3 == 0 or str(number).__contains__('3'):
           return 'fizz'
-   elif str(number).__contains__('5'):   
-          return 'buzz'
+   elif number%5 == 0 or str(number).__contains__('5'):
+          return 'buzz'   
+   
+   #if str(number).__contains__('3'):
+  #       return 'fizz'
+   elif str(number).__contains__('3') and str(number).__contains__('5'):   
+          return 'fizz buzz'
    #else:
    #     return number
     
 if __name__ == '__main__':
-   print(fizz_buzz2(13))
+   print(fizz_buzz2(53))
