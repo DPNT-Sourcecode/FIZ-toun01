63510abe4a66c40279388e6a6110b675d5c20487
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 15:59:04 +0000

    Tue Aug 27 15:59:04 UTC 2019

--- lib/temp2.py
+++ lib/temp2.py
@@ -5,4 +5,8 @@ Created on Tue Aug 27 16:54:51 2019
 @author: admin
 """
 
-print ("test")
+def greeting(name):
+   return 'Hello, '+name+'!'
+
+if __name__ == '__main__':
+   print(greeting("John"))
