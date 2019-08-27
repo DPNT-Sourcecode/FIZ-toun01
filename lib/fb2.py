d13133ea475ec76c4d5ef07f1456ae06aced7930
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 17:44:49 +0000

    Tue Aug 27 17:44:49 UTC 2019

--- lib/fb2.py
+++ lib/fb2.py
@@ -1,6 +1,6 @@
 
 def fizz_buzz2(number):
- if (n <=10 )   
+ if (number <=10):   
    if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
           return 'fizz buzz'
    if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
@@ -11,18 +11,20 @@ def fizz_buzz2(number):
           return 'buzz'   
    else:
         return number
- elif (n > 10):
+ elif (number > 10):
    
-     identical = str(n)
+     identical = str(number)[0]
+     if (str(number).count(identical)==len(str(n))):
+       dlx = ' deluxe'  
      
        if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
-          return 'fizz buzz'
+          return 'fizz buzz'+dlx
        if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
-          return 'fizz buzz'
+          return 'fizz buzz'+dlx
        elif number%3 == 0 or str(number).__contains__('3'):
-          return 'fizz'
+          return 'fizz'+dlx
        elif number%5 == 0 or str(number).__contains__('5'):
-          return 'buzz'   
+          return 'buzz'+dlx   
        else:
           return number  
 
