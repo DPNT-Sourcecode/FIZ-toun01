67759980d842580734b8ff99e6a82a5ce9aba944
sbx_user1051 <sbx_user1051@169.254.11.53>
2019-08-27 18:34:31 +0000

    Tue Aug 27 18:34:31 UTC 2019

--- lib/fb2.py
+++ lib/fb2.py
@@ -16,23 +16,23 @@ def fizz_buzz2(number):
      identical = str(number)[0]
           
      if (str(number).count(identical)==len(str(number))):
-       dlx = 'deluxe'
-     else
+       dlx = ' deluxe'
+     else:
        dlx = ''
      
      if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
-          return 'fizz buzz'+' '+dlx
+          return 'fizz buzz'+dlx
      if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
-          return 'fizz buzz'+' '+dlx
+          return 'fizz buzz'+dlx
      elif number%3 == 0 or str(number).__contains__('3'):
-          return 'fizz'+' '+dlx
+          return 'fizz'+dlx
      elif number%5 == 0 or str(number).__contains__('5'):
-          return 'buzz'+' '+dlx   
-     else:
+          return 'buzz'+dlx   
+     elif dlx=='deluxe':
           return dlx
      else:
         return number  
 
    
 if __name__ == '__main__':
-   print(fizz_buzz2(27))
+   print(fizz_buzz2(11))
