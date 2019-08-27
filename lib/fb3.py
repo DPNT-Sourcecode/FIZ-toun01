359f19e7bd30efe4d05750e885abd3693358bb6d
sbx_user1051 <sbx_user1051@169.254.64.173>
2019-08-27 19:46:51 +0000

    Tue Aug 27 19:46:51 UTC 2019

---
+++ lib/fb3.py
@@ -0,0 +1,43 @@
+
+def fizz_buzz2(number):
+ if (number <=10):   
+   if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
+          return 'fizz buzz'
+   if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'
+   elif number%3 == 0 or str(number).__contains__('3'):
+          return 'fizz'
+   elif number%5 == 0 or str(number).__contains__('5'):
+          return 'buzz'   
+   else:
+        return number
+ elif (number > 10):
+   
+     #identical = str(number)[0]
+     #c = int(identical)
+     
+     if (str(number).count(identical)==len(str(number))) and (c % 2 == 1):
+       dlx = ' fake deluxe'
+     elif ((number%3 == 0 and str(number).__contains__('3')) or (number%5 == 0 and str(number).__contains__('5'))):
+       dlx = ' deluxe'
+     else:
+       dlx = ''
+     
+     if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
+          return 'fizz buzz'+dlx
+     if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'+dlx
+     elif number%3 == 0 or str(number).__contains__('3'):
+          return 'fizz'+dlx
+     elif number%5 == 0 or str(number).__contains__('5'):
+          return 'buzz'+dlx   
+     elif dlx==' deluxe':
+          return 'deluxe'
+     elif dlx==' fake deluxe':
+          return 'fake deluxe' 
+     else:
+        return number  
+
+   
+if __name__ == '__main__':
+   print(fizz_buzz2(11))
