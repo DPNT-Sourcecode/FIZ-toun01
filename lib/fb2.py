a25e52d76cc8c8fffe397cebb16eb97b0544d1d9
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 17:14:39 +0000

    Tue Aug 27 17:14:39 UTC 2019

--- lib/fb2.py
+++ lib/fb2.py
@@ -3,6 +3,8 @@ def fizz_buzz2(number):
     
    if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
           return 'fizz buzz'
+   if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'
    elif number%3 == 0 or str(number).__contains__('3'):
           return 'fizz'
    elif number%5 == 0 or str(number).__contains__('5'):
@@ -12,8 +14,8 @@ def fizz_buzz2(number):
   #       return 'fizz'
    #elif str(number).__contains__('3') and str(number).__contains__('5'):   
     #      return 'fizz buzz'
-   #else:
-   #     return number
+   else:
+        return number
     
 if __name__ == '__main__':
    print(fizz_buzz2(546))
--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,6 +2,8 @@
 def fizz_buzz(number):
     if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
           return 'fizz buzz'
+    if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'          
     elif number%3 == 0 or str(number).__contains__('3'):
           return 'fizz'
     elif number%5 == 0 or str(number).__contains__('5'):
