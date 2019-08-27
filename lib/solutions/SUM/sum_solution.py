2ef551d6a47f8973963671a4c7950515a1f8d1cf
sbx_user1051 <sbx_user1051@169.254.160.117>
2019-08-27 13:46:17 +0000

    Tue Aug 27 13:46:17 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,11 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
-    
-def SUM(x,y):
     params = [x,y]
     c = sum(params)
     return c
 
 if __name__ == '__main__':
-    SUM(2,2)
+    compute(2,2)
