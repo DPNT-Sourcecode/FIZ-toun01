b77bd79632d8b7a813f0c8e2ca5be5600bb8bd17
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 16:06:25 +0000

    Tue Aug 27 16:06:25 UTC 2019

--- lib/temp2.py
+++ lib/temp2.py
@@ -5,8 +5,10 @@ Created on Tue Aug 27 16:54:51 2019
 @author: admin
 """
 
-def greeting(name):
-   return 'Hello, '+name+'!'
+def fizz_buzz(number):
+    
+   if number%3 == 0:
+          return 'fizz'
 
 if __name__ == '__main__':
-   print(greeting("John"))
+   print(fizz_buzz(2))
