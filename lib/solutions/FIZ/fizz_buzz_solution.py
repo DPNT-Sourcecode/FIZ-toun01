757a62ded21ad49da206a721ed501e71bea710de
sbx_user1051 <sbx_user1051@169.254.152.197>
2019-08-27 17:59:01 +0000

    Tue Aug 27 17:59:01 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,6 +1,7 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-if (number <=10):   
+
+ if (number <=10):   
    if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
           return 'fizz buzz'
    if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
@@ -13,19 +14,20 @@ if (number <=10):
         return number
  elif (number > 10):
    
- identical = str(number)[0]
-   if (str(number).count(identical)==len(str(number))):
-        dlx = 'deluxe'  
-        
-        if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
-           return 'fizz buzz'+' '+dlx
-        if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
-           return 'fizz buzz'+' '+dlx
-        elif number%3 == 0 or str(number).__contains__('3'):
-           return 'fizz'+' '+dlx
-        elif number%5 == 0 or str(number).__contains__('5'):
-           return 'buzz'+' '+dlx   
-        else:
-           return dlx
-    else:
-       return number  
+     identical = str(number)[0]
+          
+     if (str(number).count(identical)==len(str(number))):
+       dlx = 'deluxe'  
+     
+       if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
+          return 'fizz buzz'+' '+dlx
+       if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'+' '+dlx
+       elif number%3 == 0 or str(number).__contains__('3'):
+          return 'fizz'+' '+dlx
+       elif number%5 == 0 or str(number).__contains__('5'):
+          return 'buzz'+' '+dlx   
+       else:
+          return dlx
+     else:
+        return number  
