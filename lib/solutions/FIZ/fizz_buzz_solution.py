859827195616b885ffa2360a03cc6c474acfa6c4
sbx_user1051 <sbx_user1051@169.254.11.53>
2019-08-27 18:38:26 +0000

    Tue Aug 27 18:38:26 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -21,15 +21,15 @@ def fizz_buzz(number):
      else:
        dlx = ''
        
-       if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
-          return 'fizz buzz'+' '+dlx
-       if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
-          return 'fizz buzz'+' '+dlx
-       elif number%3 == 0 or str(number).__contains__('3'):
-          return 'fizz'+' '+dlx
-       elif number%5 == 0 or str(number).__contains__('5'):
-          return 'buzz'+' '+dlx   
-       else:
-          return dlx
+     if (number%3 == 0 and number%5 == 0) or (str(number).__contains__('3') and str(number).__contains__('5')): 
+          return 'fizz buzz'+dlx
+     if (number%3 == 0 and str(number).__contains__('5')) or (number%5 == 0 and str(number).__contains__('3')): 
+          return 'fizz buzz'+dlx
+     elif number%3 == 0 or str(number).__contains__('3'):
+          return 'fizz'+dlx
+     elif number%5 == 0 or str(number).__contains__('5'):
+          return 'buzz'+dlx   
+     elif dlx==' deluxe':
+          return 'deluxe'
      else:
         return number  
