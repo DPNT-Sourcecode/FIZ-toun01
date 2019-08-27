06f1b689c69a384b3459a8212bceff398bd60f19
sbx_user1051 <sbx_user1051@169.254.88.253>
2019-08-27 08:11:32 +0000

    Tue Aug 27 08:11:32 UTC 2019

---
+++ lib/sum_r1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 08:43:30 2019
+
+@author: admin
+"""
+
+def sum_r1():
+    params = [2,2]
+    c = sum(params)
+    return c
+
+if __name__ == '__main__'
+    sum_r1()
